c2fc417edc04d313a206c08c987bab189d2bded5
Lodewijk5150 <kingnerfpro@gmail.com>
2023-02-04 11:24:11 -0700

    changes with terminal

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="text_output">
+        <div className="line">This is a paragraph.</div>
+        <div className="line">This is a paragraph number 2.</div>
+        
+      </p>
+      <input type='text' className="text_input"/>
     </div>
   );
 }
