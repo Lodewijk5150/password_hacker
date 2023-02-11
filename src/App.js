f863373ccc10a66071b00e3653e418fe424dffee
Lodewijk5150 <kingnerfpro@gmail.com>
2023-02-11 11:06:48 -0700

    state variables

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [body,setBody] = useState(['hello.','hi','My name is Tiago'])
   return (
     <div className="App">
       <p className="text_output">
-        <div className="line">This is a paragraph.</div>
-        <div className="line">This is a paragraph number 2.</div>
-        
+        {body.map((line_text)=>{
+          return(
+            <div className="line">{line_text}</div>
+          )
+        })}        
       </p>
       <input type='text' className="text_input"/>
+      <button onClick={()=>{
+        let new_body = [...body]
+        new_body.push('This is a great text.')
+        setBody (new_body)
+      }}>click me!</button>
     </div>
   );
 }
